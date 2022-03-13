353a76c49495e32f4f0f17946c947725be52a27c
Derrick Wong <derrick.chwong@gmail.com>
2022-03-14 01:06:08 +0800

    first commit

---
+++ web.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Let the battle begin!');
+});
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  const moves = ['F', 'T', 'L', 'R'];
+  res.send(moves[Math.floor(Math.random() * moves.length)]);
+});
+
+app.listen(process.env.PORT || 8080);
